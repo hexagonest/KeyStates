f80ae7b9273f7e0255cc5f87a8494c0fdf4db4f8
Jacques Dancause <jacques.dancause@gmail.com>
2015-11-11 20:12:40 +1000

    initial

---
+++ LOVEScript.js
@@ -0,0 +1,40 @@
+//probably shouldn't do this. But it saves time.
+$ = function (id) { return document.getElementById(id); }
+var love = {};
+
+var keysDown = {};
+var canvas;
+var ctx;
+
+function initLoveScript(can){
+    canvas = can;
+    ctx = canvas.getContext("2d");
+}
+
+canvas.addEventListener("keydown", onKeyDown, true);
+canvas.addEventListener("keyup", onKeyUp, true);
+
+function onKeyDown(e) {
+    keysDown[e.keyCode] = true;
+    console.log("key down.");
+    console.log(keysDown);
+}
+
+function onKeyUp(e) {
+    keysDown[e.keyCode] = false;
+    console.log("key UP NIGA");
+    console.log(keysDown);
+}
+
+love.keyDown = function() {
+    if(keysDown.hasOwnProperty(id))
+        return keysDown[id];
+    
+    return false;
+}
+
+love.load();
+love.update();
+love.draw();
+setInterval(function(){love.update();love.draw();},1);
+
