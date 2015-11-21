a666b52503273224668beed414f86dac40ad4793
Jacques Dancause <jacques.dancause@gmail.com>
2015-11-21 11:27:00 +1000

    Update thing

---
+++ KeyStates.js
@@ -0,0 +1,22 @@
+var Keys = function(element) {
+    this.element = element;
+    this.keysDown = {};
+    
+    this.element.addEventListener("keydown", this.keys.onKeyDown, true);
+    this.element.addEventListener("keyup", this.keys.onKeyUp, true);
+};
+
+Keys.prototype.keyDown = function(id) {
+    if(this.keysDown.hasOwnProperty(id))
+        return this.keysDown[id];
+    
+    return false;
+}
+
+Keys.prototype.onKeyDown = function(e) {
+    this.keysDown[e.keyCode] = true;
+}
+
+Keys.prototype.onKeyUp = function(e) {
+    this.keysDown[e.keyCode] = false;
+}
